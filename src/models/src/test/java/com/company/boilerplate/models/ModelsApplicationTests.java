67b69892461b7018ad0548d867b9f0c1a23e243f
Lucian Sturião <lucian.sturiao@zoom.com.br>
2018-12-07 18:17:48 -0200

    adding projects

---
+++ src/models/src/test/java/com/company/boilerplate/models/ModelsApplicationTests.java
@@ -0,0 +1,16 @@
+package com.company.boilerplate.models;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ModelsApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
