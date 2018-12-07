0f3b4ef4463b5331f9b46c85a2fd17c223693189
Lucian Sturião <lucian.sturiao@zoom.com.br>
2018-12-07 17:22:16 -0200

    first blood

--- src/api/src/main/java/com/company/boilerplate/api/api/ApiApplication.java
+++ src/api/src/main/java/com/company/boilerplate/api/api/ApiApplication.java
@@ -1,12 +1,12 @@
-package com.boilerplate.demo;
+package com.company.boilerplate.api.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class ApiApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(ApiApplication.class, args);
 	}
 }
--- src/api/src/test/java/com/company/boilerplate/api/api/ApiApplicationTests.java
+++ src/api/src/test/java/com/company/boilerplate/api/api/ApiApplicationTests.java
@@ -1,4 +1,4 @@
-package com.boilerplate.demo;
+package com.company.boilerplate.api.api;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class DemoApplicationTests {
+public class ApiApplicationTests {
 
 	@Test
 	public void contextLoads() {
