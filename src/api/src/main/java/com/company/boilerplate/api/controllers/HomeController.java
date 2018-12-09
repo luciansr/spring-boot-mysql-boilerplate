3dc46398ebeda6f4a4862f5934dc1057910ab9ce
Lucian Sturião <luciansr@gmail.com>
2018-12-09 03:11:14 -0200

    adding HomeController

--- src/api/src/main/java/com/company/boilerplate/api/controllers/HomeController.java
+++ src/api/src/main/java/com/company/boilerplate/api/controllers/HomeController.java
@@ -1,4 +1,12 @@
 package com.company.boilerplate.api.controllers;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class HomeController {
+    @RequestMapping("/")
+    public String index() {
+        return "Greetings from Spring Boot!";
+    }
 }
