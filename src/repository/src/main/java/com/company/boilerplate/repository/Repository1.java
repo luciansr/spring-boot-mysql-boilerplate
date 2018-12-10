048a547761b3b552d9bb3014e7fb8a0c7004f58d
Lucian Sturião <luciansr@gmail.com>
2018-12-10 00:04:01 -0200

    adding files and testing encapsulation

--- src/repository/src/main/java/com/company/boilerplate/repository/Repository1.java
+++ src/repository/src/main/java/com/company/boilerplate/repository/Repository1.java
@@ -1,4 +1,10 @@
 package com.company.boilerplate.repository;
 
+import com.company.boilerplate.repository.domain.User;
+
 public class Repository1 {
+
+    public User getUser() {
+        return null;
+    }
 }
