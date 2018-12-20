2845a9cd0e5d105fd65148801448b5d91e6b1ce8
Lucian Sturião <luciansr@gmail.com>
2018-12-20 00:13:13 -0200

    removing unused classes

--- src/main/java/com/company/boilerplate/config/auth/WebSecurityConfig.java
+++ src/main/java/com/company/boilerplate/config/auth/WebSecurityConfig.java
@@ -1,7 +1,6 @@
 package com.company.boilerplate.config.auth;
 
 
-import com.company.boilerplate.services.auth.AuthUserDetailsService;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
@@ -36,46 +35,23 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Value("${jwt.config.issuer}")
     private String ISSUER;
 
-    private static final String SIGN_UP_URL = "/login";
-
-    private AuthUserDetailsService authUserDetailsService;
-
     @Bean
     public BCryptPasswordEncoder bCryptPasswordEncoder() {
         return new BCryptPasswordEncoder();
     }
 
-    public WebSecurityConfig(AuthUserDetailsService authUserDetailsService) {
-        this.authUserDetailsService = authUserDetailsService;
-    }
-
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.cors().and().csrf().disable().authorizeRequests()
-                //.antMatchers(HttpMethod.POST, SIGN_UP_URL).permitAll()
                 .anyRequest().permitAll()
                 .and()
-//                .addFilter(new JWTAuthenticationFilter(authenticationManager(), AUTHORIZATION_HEADER, EXPIRATION_TIME, TOKEN_PREFIX, SECRET))
                 .addFilter(new JWTAuthorizationFilter(authenticationManager(),
                         AUTHORIZATION_HEADER,
                         TOKEN_PREFIX,
                         SECRET,
                         AUDIENCE,
                         ISSUER))
-                // this disables session creation on Spring Security
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and().exceptionHandling().authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED));
-    }
-
-    @Override
-    public void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.userDetailsService(authUserDetailsService).passwordEncoder(bCryptPasswordEncoder());
-    }
-
-    @Bean
-    CorsConfigurationSource corsConfigurationSource() {
-        final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", new CorsConfiguration().applyPermitDefaultValues());
-        return source;
     }
 }
