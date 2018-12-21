da9edc4be1737068bd619bf5f9da262343bfd6cb
Lucian Sturião <luciansr@gmail.com>
2018-12-21 00:25:07 -0200

    basic swagger-ui working

--- src/main/java/com/company/boilerplate/controllers/HomeController.java
+++ src/main/java/com/company/boilerplate/controllers/HomeController.java
@@ -1,28 +1,29 @@
 package com.company.boilerplate.controllers;
 
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HomeController {
-    @RequestMapping("/")
+    @GetMapping("/")
     public String index() {
         return "Greetings from Spring Boot!";
     }
 
-    @RequestMapping("/anonymous")
+    @GetMapping("/anonymous")
     public String anonymous() {
         return "Anonymous";
     }
 
-    @RequestMapping("/logged")
+    @GetMapping("/logged")
     @PreAuthorize("isAuthenticated()")
     public String logged() {
         return "logged";
     }
 
-    @RequestMapping("/login")
+    @GetMapping("/login")
     public String login() {
         return "login";
     }
